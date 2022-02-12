8320b56a42f7d8fe5d44f512f128d1919051b2f5
yaroslavh <yarosalvh@wix.com>
2022-02-12 21:11:56 +0200

    initial

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "rozklad"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
+
+idePackagePrefix := Some("rozklad")
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+package rozklad
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("hello")
+  }
+}
