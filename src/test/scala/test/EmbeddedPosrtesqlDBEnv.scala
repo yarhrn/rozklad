7059ca9104f6b3f5c7bacbf032539676289a8a5d
yaroslavh <yarosalvh@wix.com>
2022-08-17 13:26:19 +0200

    wait till connection is available

--- src/test/scala/test/EmbeddedPosrtesqlDBEnv.scala
+++ src/test/scala/test/EmbeddedPosrtesqlDBEnv.scala
@@ -7,8 +7,11 @@ import doobie.Transactor
 import org.scalatest.{BeforeAndAfterAll, Suite}
 import org.testcontainers.utility.DockerImageName
 
-import java.sql.DriverManager
+import java.sql.{Connection, DriverManager}
 import doobie.implicits._
+
+import scala.util.Try
+
 
 trait EmbeddedPosrtesqlDBEnv extends BeforeAndAfterAll with Shortcuts {
   self: Suite =>
@@ -21,9 +24,21 @@ trait EmbeddedPosrtesqlDBEnv extends BeforeAndAfterAll with Shortcuts {
   }
 
   def refreshScheme() = {
-    Thread.sleep(5000)
-    val connection =
-      DriverManager.getConnection(postgres.jdbcUrl, postgres.username, postgres.password)
+    var attempt: Int = 1
+    var connectionOption: Option[Connection] = None
+    while (attempt < 10){
+      Thread.sleep(2000)
+      connectionOption = Try(DriverManager.getConnection(postgres.jdbcUrl, postgres.username, postgres.password)).toOption
+      if(connectionOption.isDefined){
+        attempt = 100
+      } else {
+        attempt = attempt + 1
+      }
+    }
+
+    val connection = connectionOption.getOrElse(throw new RuntimeException("cant obtain connection to test DB"))
+
+
     val statement = connection.createStatement()
     val source = io.Source.fromResource("./scheme.sql")
     statement.execute(source.getLines().mkString("\n"))
